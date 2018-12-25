ce60f9fc1f22708e4d3037df2aee2a870d66e8ec
Dragos Bulugean <dragos.bulugean@gmail.com>
2018-12-26 00:22:50 +0200

    Test.

--- api/api.js
+++ api/api.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("*", (req, res) => res.send("Hello World!"));
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("database", null, null, {
+  dialect: "sqlite",
+  storage: "db.sqlite"
+});
+
+//  MODELS
+var User = sequelize.define("User", {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+//  SYNC SCHEMA
+sequelize.sync({ force: true }).then(
+  function(err) {
+    console.log("It worked!");
+  },
+  function(err) {
+    console.log("An error occurred while creating the table:", err);
+  }
+);
+
+app.get("*", (req, res) => res.send("Hello World! " + new Date()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
